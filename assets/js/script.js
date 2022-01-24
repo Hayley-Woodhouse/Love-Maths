c39d79b7ecaed99cf40678c31c21d22a3ed76fc6
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-01-24 09:28:00 +0000

    Add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! you got it right! :D");
+        incrementScore();
     } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -71,17 +73,24 @@ function calculateCorrectAnswer() {
         alert(`unimplemented operator ${operator}`);
         throw`inimplemented operator ${operator}.Aborting!`;
     }
-
-
 }
 
-
+/**
+ *  Get the current score from the DOM and increment it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = oldScore +1;
 }
 
+/**
+ *  Get the current tally of incorrect answers from the DOM and increment it by 1
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = oldScore +1;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
